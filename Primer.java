d420b34a708efe878c884c671b28ccbf555676b3
GabrielaMendozal <mendozalizethg@gmail>
2022-02-23 12:59:58 -0500

    first commit

---
+++ Primer.java
@@ -0,0 +1,12 @@
+public class Primer{
+    public static void main(String args[]){
+    String nombre= "Coding Dojo";
+    long edad = 100;
+    String ciudad= "Burbank, CA";
+
+
+    System.out.println("Mi nombre es " + nombre);
+    System.out.println("Tengo " + edad + " años de edad");
+    System.out.println("Mi ciudad natal es " + ciudad);
+    }
+}
